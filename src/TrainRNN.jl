4a3eccf24660d34d3607190dbc5a49de84ffb19e
Brad Safnuk <safnuk@gmail.com>
2018-04-26 20:38:31 -0400

    Allow RNN to train

--- src/TrainRNN.jl
+++ src/TrainRNN.jl
@@ -1,10 +1,13 @@
-using BSON: @load
-using JLD
+import BSON
+import JLD
 using Flux
+using Flux: back!, crossentropy
 
 feature_model_file = "resnet-gn-16-10-best.bson"
+trainpath = "labeled"
+testpath = "labeled_test"
 out_features = 12*20*640
-N = 15
+N = 10
 
 function load_data(basedir)
     data = []
@@ -17,7 +20,7 @@ function load_data(basedir)
         for (root, dirs, files) in walkdir(path)
             for file in files
                 filepath = joinpath(root, file)
-                x = load(filepath)
+                x = JLD.load(filepath)
                 x["frames"] = x["frames"] ./ 1.0
                 x["frames"][:, :, 1, :] = x["frames"][:, :, 1, :] ./ 255.0
                 y = copy(label)
@@ -28,7 +31,7 @@ function load_data(basedir)
     return data
 end
 
-# @load feature_model_file feature_model
+# BSON.@load feature_model_file feature_model
 
 feature_model = Chain(
     Conv((3,3), 1=>640, relu; pad=(1, 1)),
@@ -61,3 +64,54 @@ function model(x)
     softmax(predictor(state))
 end
 
+function accuracy(data)
+    mu = 0.0
+    n = 0
+    for (x, y) in data
+        n += 1
+        out = model(x)
+        y = gpu(y)
+        loss += crossentropy(out, y) 
+        mu += mean(argmax(out) .== argmax(y))
+    end
+    loss / n, mu / n
+end
+
+traindata = load_data(trainpath)
+testdata = load_data(testpath)
+loss(x, y) = crossentropy(model(x), y)
+opt = ADAM(params(downsample, scanner, predictor))
+
+macro interrupts(ex)
+  :(try $(esc(ex))
+    catch e
+      e isa InterruptException || rethrow()
+      throw(e)
+    end)
+end
+
+function train(loss, data, opt, freq=10)
+    loss_tracker = []
+    for (n, d) in enumerate(data)
+        l = loss(d...)
+        push!(loss_tracker, l)
+        isinf(l) && error("Loss is Inf")
+        isnan(l) && error("Loss is NaN")
+        @interrupts back!(l)
+        opt()
+        if n % freq == 0
+            avg_loss = mean(loss_tracker[end-freq+1:end])
+            @show avg_loss
+        end
+    end
+    println("Epoch average loss: $(mean(loss_tracker))")
+end
+
+function run_rnn(loss, opt, traindata, testdata, end_epoch, start_epoch=1)
+    for n in start_epoch:end_epoch
+        println("Epoch $n ==============")
+        train(loss, traindata, opt)
+        @show accuracy(testdata)
+        BSON.@save "rnn_models.bson" downsample scanner predictor
+    end
+end
