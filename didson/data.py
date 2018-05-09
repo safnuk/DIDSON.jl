09290db35585cdd4977d3cc6cf9d9b05061be95c
Brad Safnuk <safnuk@gmail.com>
2018-05-09 06:22:43 -0400

    Create imagenet datalodaer

--- didson/data.py
+++ didson/data.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pickle
 
 import numpy as np
 import torch
@@ -57,3 +58,47 @@ class SequenceDataset(Dataset):
             return (self.lamprey[idx - self.lamprey_offset], self.LAMPREY)
         else:
             return (self.other[idx - self.other_offset], self.OTHER)
+
+
+class ImagenetDataset(Dataset):
+    def __init__(self, path):
+        path = Path(path)
+        self.batches = []
+        self.labels = []
+        self.lengths = []
+        for filepath in path.glob("*"):
+            x, y = self.load_databatch(filepath)
+            self.batches.append(x)
+            self.labels.append(y)
+            self.lengths.append(x.shape[0])
+
+    def __len__(self):
+        return np.sum(self.lengths)
+
+    def __getitem__(self, idx):
+        batch = 0
+        while idx >= self.lengths[batch]:
+            idx -= self.lengths[batch]
+            batch += 1
+        x = self.batches[batch][idx]
+        y = self.labels[batch][idx]
+        x = x.astype(np.float32) / 255.0
+        x = torch.from_numpy(x)
+        return x, y
+
+    def load_databatch(self, filepath, img_size=32):
+        with filepath.open('rb') as f:
+            d = pickle.load(f)
+        x = d['data']
+        y = d['labels']
+
+        # Labels are indexed from 1, shift it so that indexes start at 0
+        y = [i-1 for i in y]
+
+        img_size2 = img_size * img_size
+
+        x = np.dstack(
+            (x[:, :img_size2], x[:, img_size2:2*img_size2], x[:, 2*img_size2:]))
+        x = x.reshape((x.shape[0], img_size, img_size, 3)).transpose(0, 3, 1, 2)
+
+        return x, y
